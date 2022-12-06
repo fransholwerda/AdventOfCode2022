9192b874cd2b1bc479ddab763071430d2dd01b25
Frans Holwerda <fholwerd@f1r4s20.codam.nl>
2022-12-06 11:37:13 +0100

    Day 6 complete

---
+++ Day06/star_one.py
@@ -0,0 +1,21 @@
+def check_unique(marker):
+	print(marker)
+	for char in marker:
+		if marker.count(char) > 1:
+			return False
+	return True
+
+with open("input.txt", 'r') as fd:
+	inp = fd.readlines()
+
+marker = []
+for i in range(len(inp[0])):
+	if len(marker) < 4:
+		marker.append(inp[0][i])
+	else:
+		marker.pop(0)
+		marker.append(inp[0][i])
+	if len(marker) == 4:
+		if check_unique(marker) == True:
+			print(i+1)
+			break
